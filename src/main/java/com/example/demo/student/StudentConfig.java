744c432b468b7a46b2dc4b94466044fe0707b7ff
Mohammadassaf10 <mhamad234assaf@gmail.com>
2024-05-06 18:23:27 +0300

    - Linked project with postgres database. - Implement get students api.

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,32 @@
+//package com.example.demo.student;
+//
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import java.time.LocalDate;
+//import java.time.Month;
+//import java.util.List;
+//
+//
+//@Configuration
+//public class StudentConfig {
+//    @Bean
+//    CommandLineRunner commandLineRunner(StudentRepository studentRepository) {
+//        return args -> {
+//            Student mohamad = new Student(
+//                    "Mohamad",
+//                    "Mohamad@gmail.com",
+//                    LocalDate.of(2001, Month.OCTOBER, 12),
+//                    23
+//            );
+//            Student alex = new Student(
+//                    "Alex",
+//                    "Alex@gmail.com",
+//                    LocalDate.of(2004, Month.OCTOBER, 12),
+//                    20
+//            );
+//            studentRepository.saveAll(List.of(mohamad, alex));
+//        };
+//    }
+//}
